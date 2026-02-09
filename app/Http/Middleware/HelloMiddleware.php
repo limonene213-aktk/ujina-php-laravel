ed5c2d7a1823531a2fca5ce75de61f6c3425e1da
limonene213u <limonene@hirohsima-aktk.com>
2026-02-09 14:34:46 +0900

    HelloMiddlewareを作成した

---
+++ app/Http/Middleware/HelloMiddleware.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class HelloMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+
+    public function handle(Request $request, Closure $next): Response
+    {
+        {
+            $data = [
+                ['name'=>'jua', 'mail'=>'jua@daisuki.com'],
+                ['name'=>'jack', 'mail'=>'jack@blackjack.com'],
+                ['name'=>'pinoko', 'mail'=>'pinoko@dayonosa.com'],
+            ];
+
+            //mergeメソッドは、フォーム送信などで送ら得る値に新しい値を追加して合成（merge）するものです。
+            //これにより、dataという項目で$dataの内容が追加されます。
+            //コントローラーガワでは、$request->dataでこの値を取り出すことができるようになります。
+            $response->merge(['data'=>$data]);
+
+            return $nest($request);
+        }
+        return $next($request);
+    }
+}
