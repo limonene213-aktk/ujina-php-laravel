6f20eafe3e47daeb7eb4adc6f5db601b15390b24
limonene213u <limonene@hirohsima-aktk.com>
2026-02-09 13:31:44 +0900

    修正

--- app/View/Components/Message.php
+++ app/View/Components/Message.php
@@ -4,43 +4,28 @@ namespace App\View\Components;
 
 use Closure;
 use Illuminate\Contracts\View\View;
-use Illuminate\Support\Facades\Http;
 use Illuminate\View\Component;
+use Illuminate\Support\Facades\Http;
 
 class Message extends Component
 {
-    /**
-     * Create a new component instance.
-     */
-
-    private $id;//追加
-    private $data; //外部APIから取得したデータを格納するプロパティを定義しておく
+    private $id;
+    private $data;
     private $msg;
 
-    public function __construct($id = 1) // ← $idを引数として受け取る（デフォルト値は1）
+    public function __construct($id = 1)
     {
-        $this->msg = 'ランダムなPOSTデータを表示するよ';//追加
-        $this->id = $id ?? 1; // nullの場合は1を使用
-
-        $response = Http::get('https://jsonplaceholder.typicode.com/posts/'. $this->id) ;//外部APIにアクセス
-        //Http::get()はLaravelのHTTPクライアント機能を使って外部APIにGETリクエストを送るメソッドで、使い方は以下の通り
-        //Http::get('URL')の形で使用し、指定したURLにGETリクエストを送信します。"."で繋いで変数も使えます。
-        //use Illuminate\Support\Facades\Http;を宣言しておく必要がある。
-
-        $this->data = $response->json(); //json形式でデータを取得
+        $this->msg = 'ランダムなPOSTデータを表示します。';
+        $this->id = $id;
+        $response = Http::get('https://jsonplaceholder.typicode.com/posts/' . $this->id);
+        $this->data = $response->json();
     }
 
-    /**
-     * Get the view / contents that represent the component.
-     */
     public function render(): View|Closure|string
     {
-        //return view('components.message'); //componentsの中のmessage.blade.phpを参照して返すだけの処理
-        //書き換え：外部からコンポーネントに値を渡す
-
-        return view('components.message', [
+        return view('components.message',[
             'id' => $this->id,
-            'data'=> $this->data,
+            'data' => $this->data,
             'msg' => $this->msg
         ]);
     }
