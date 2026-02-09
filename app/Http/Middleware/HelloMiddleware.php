ed82326a426776c3ae912190bebab5ebf9a6202a
limonene213u <limonene@hirohsima-aktk.com>
2026-02-09 15:19:08 +0900

    タイポ修正

--- app/Http/Middleware/HelloMiddleware.php
+++ app/Http/Middleware/HelloMiddleware.php
@@ -18,20 +18,17 @@ class HelloMiddleware
 
     public function handle(Request $request, Closure $next): Response
     {
-        {
-            $data = [
-                ['name'=>'jua', 'mail'=>'jua@daisuki.com'],
-                ['name'=>'jack', 'mail'=>'jack@blackjack.com'],
-                ['name'=>'pinoko', 'mail'=>'pinoko@dayonosa.com'],
-            ];
+        $data = [
+            ['name'=>'jua', 'mail'=>'jua@daisuki.com'],
+            ['name'=>'jack', 'mail'=>'jack@blackjack.com'],
+            ['name'=>'pinoko', 'mail'=>'pinoko@dayonosa.com'],
+        ];
 
-            //mergeメソッドは、フォーム送信などで送られる値に新しい値を追加して合成（merge）するものです。
-            //これにより、dataという項目で$dataの内容が追加されます。
-            //コントローラーの側では、$request->dataでこの値を取り出すことができるようになります。
-            $response->merge(['data'=>$data]);
+        //mergeメソッドは、フォーム送信などで送られる値に新しい値を追加して合成（merge）するものです。
+        //これにより、dataという項目で$dataの内容が追加されます。
+        //コントローラーの側では、$request->dataでこの値を取り出すことができるようになります。
+        $request->merge(['data'=>$data]);
 
-            return $nest($request);
-        }
         return $next($request);
     }
 }
