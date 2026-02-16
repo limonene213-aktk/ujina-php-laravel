f29f8717df8fc48640cd44eb39acc1f60e83f267
limonene213u <limonene@hirohsima-aktk.com>
2026-02-16 15:23:05 +0900

    P.176まで

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -12,24 +12,34 @@
 @section('content')
 <p>{{$msg}}</p>
 
+@if (count($errors) > 0)
+<div>
+    <ul>
+        @foreach ($errors->all() as $error)
+            <li>{{$error}}</li>
+        @endforeach
+    </ul>
+</div>
+@endif
+
 <form action="/hello" method="post">
     @csrf
     <div>
         <labey style="display:inline-block; width:75px;"
         for="name">name:</label>
-        <input type="text" name = "name">
+        <input type="text" name = "name" vale = "{{old('name')}}">
     </div>
 
     <div>
         <labey style="display:inline-block; width:75px;"
         for="name">mail:</label>
-        <input type="text" name = "name">
+        <input type="text" name = "mail" value = "{{old('mail')}}">
     </div>
 
     <div>
         <labey style="display:inline-block; width:75px;"
         for="name">age:</label>
-        <input type="text" name = "name">
+        <input type="text" name = "age" value = "{{old('age')}}">
         <input type="submit" value="send">
     </div>
 </form>
