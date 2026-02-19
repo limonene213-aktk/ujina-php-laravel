6d3735fb00c7b87f3640a27084afffc244362ec4
limonene213u <limonene@hirohsima-aktk.com>
2026-02-19 14:59:53 +0900

    FormRequestを継承して拡張したHelloRequestを作り、メソッドをHelloControllerから移動した

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request; //お願い
 use Illuminate\Http\Response; //お返事
 
+use App\Http\Requests\HelloRequest; //お願い（令和最新版）
+
 class HelloController extends Controller{
 
     public function index(Request $request) //引数を渡して
@@ -16,15 +18,20 @@ class HelloController extends Controller{
     ['msg'=>'フォームを入力してね：']);
     }
 
-    public function post(Request $request)
+    public function post(HelloRequest $request)
     {
-        $validate_rule = [
-            'name' => 'alpha:ascii', //alpha:asciiでASCII英字のみ許可（alphaだけだとUnicode＝日本語もOKになる）
-            'mail'=> 'email',
-            'age' => 'numeric|between:0,150', //numericは「数字である」、betweenは「～と・・・の間」（パイプ前後のスペースは不要）
-        ];
+        /*
+        すでにHelloRequestがここに到達した時点でバリデーションは終了済み
+        なので、ここで新たにバリデーションをする必要がない！
+        */
+        
+        //$validate_rule = [
+        //    'name' => 'alpha:ascii', //alpha:asciiでASCII英字のみ許可（alphaだけだとUnicode＝日本語もOKになる）
+        //    'mail'=> 'email',
+        //    'age' => 'numeric|between:0,150', //numericは「数字である」、betweenは「～と・・・の間」（パイプ前後のスペースは不要）
+        //];
 
-        $request->validate($validate_rule);
+        //$request->validate($validate_rule);
 
         return view('hello.index',['msg'=>'正しく入力できました！！']);
     }
