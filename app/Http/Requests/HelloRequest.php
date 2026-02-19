8da7ac1841ac27fcb33d02318a68d8a9f74e42f8
limonene213u <limonene@hirohsima-aktk.com>
2026-02-19 15:15:43 +0900

    エラーメッセージを日本語に変更

--- app/Http/Requests/HelloRequest.php
+++ app/Http/Requests/HelloRequest.php
@@ -33,4 +33,18 @@ class HelloRequest extends FormRequest
             'age' => 'required|numeric|between:0,150', 
         ];
     }
+
+    public function messages(): array
+    {
+        return [
+            'name.required'=>'必須項目です',
+            'mail.required'=>'必須項目です',
+            'age.required'=>'年齢は隠せませんよ？（暗黒微笑）',
+            'mail.email'=>'emailの形式で入力してください',
+            'age.numeric'=>'年齢は整数で入力してください',
+            'age.between'=>'そんな年齢のわけないやろ？',
+        ];
+    }
+
 }
+
